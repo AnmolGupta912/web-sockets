6b130b10206762fb7e8875839d4985acf4b5c2b7
AnmolGupta912 <pfyplannedforu@gmail.com>
2026-09-24 01:59:48 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+import http from "node:http"
+import fs from "node:fs"
+import path from "node:path"
+import {WebSocketServer} from 'ws'
+
+const PORT = process.env.PORT ?? 9000
+
+
+const httpServer = http.createServer(async function(req, res) {
+    const indexFile =  await fs.promises.readFile(path.resolve("./index.html"), 'utf-8', () => console.log("reading html file"))
+    // console.log(indexFile)
+    res.setHeader('Content-type', "text/html")
+    return res.end(indexFile)
+})
+const wsSocket = new WebSocketServer({server: httpServer})
+
+wsSocket.on("connection", (webSocket) => {
+    console.log(`WebSocket connection...`)
+
+    webSocket.on("message", (data) => {
+        console.log(`websocket Server message recv.. : ${data.toString()}`)
+    })
+
+})
+
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
